48a861810a516a86f178464dfc79c7176a64afbf
michaellintl <42783202+michaellintl@users.noreply.github.com>
2024-12-02 11:02:19 +0100

    Added like and dislike button

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,16 +1,52 @@
-
 import React, { Component } from 'react';
 import './ContentRating.css';
 
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      totalRatings: 0
+    };
+    // Bind the class methods to the component instance
+    this.handleLike = this.handleLike.bind(this);
+    this.handleDislike = this.handleDislike.bind(this);
   }
+
+  // Class method to handle increasing likes
+  handleLike() {
+    this.setState((prevState) => ({
+      likes: prevState.likes + 1,
+      totalRatings: prevState.totalRatings + 1
+    }));
+  }
+
+  // Class method to handle increasing dislikes
+  handleDislike() {
+    this.setState((prevState) => ({
+      dislikes: prevState.dislikes + 1,
+      totalRatings: prevState.totalRatings + 1
+    }));
+  }
+
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+      <>
+        <h1>Text Content Rating</h1>
+        <div className='content-rating'>
+          <p>Text</p>
+          <div className='rating-buttons'>
+            <button className="like-button" onClick={this.handleLike}>
+              Like ({this.state.likes})
+            </button>
+            <button className="dislike-button" onClick={this.handleDislike}>
+              Dislike ({this.state.dislikes})
+            </button>
+            <p>Ratings ({this.state.totalRatings})</p>
+          </div>
+        </div>
+      </>
     );
   }
 }
